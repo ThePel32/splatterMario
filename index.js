f51ad9ca0ede18044b1fb38a3968dcdc5510d210
ThePel <theo.pelven32@gmail.com>
2025-05-28 14:34:28 +0200

    creation personnage

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-console.log(ctx);
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+
+        this.width = 30
+        this.height = 30
+    }
+
+    draw() {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(
+            this.position.x,
+            this.position.y, 
+            this.width, 
+            this.height
+        )
+    }
+
+    update() {
+        this.draw();
+        this.position.y += this.velocity.y;
+    }
+}
+
+const player = new Player();
+player.update();
+
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.update();
+}
+
+animate();
